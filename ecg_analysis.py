855993360a6075ce80e295834a191516358e77bc
Canyon Duncan <canyon.duncan@duke.edu>
2020-06-08 18:15:16 -0400

    Finished read_input() so that it now appends 'good' data to two lists: time and volt. These two lists are returned. Also added logging to note when bad data points occur

--- ecg_analysis.py
+++ ecg_analysis.py
@@ -2,6 +2,10 @@ import logging
 import math
 
 
+logging.basicConfig(filename='bad_data.log',
+                    level=logging.INFO)
+
+
 def output_file(data):
     pass
 
@@ -40,32 +44,40 @@ def check_data(temp_time, temp_volt):
         return False
     elif is_a_number(temp_volt) is False:
         return False
-    elif math.isnan(temp_time) is True:
+    elif math.isnan(float(temp_time)) is True:
         return False
-    elif math.isnan(temp_volt) is True:
+    elif math.isnan(float(temp_volt)) is True:
         return False
     else:
         return True
 
 
 def read_input(filename):
-
+    time = list()
+    volt = list()
     with open(filename, 'r') as f:
         temp_line = f.readline()
-        while temp_line != " \n":
+        while temp_line != "":
             temp_time, temp_volt = split_data(temp_line)
             temp_check = check_data(temp_time, temp_volt)
             if temp_check is True:
-                temp_time = int(temp_time)
-                temp_volt = int(temp_volt)
-
-    return
+                temp_time = float(temp_time)
+                time.append(temp_time)
+                temp_volt = float(temp_volt)
+                volt.append(temp_volt)
+            else:
+                logging.error('Bad data point,'
+                              'skipping to next line')
+            temp_line = f.readline()
+    return time, volt
 
 
 def interface():
     filename = input("Please enter the filename: ")
-    ecg_raw_data = read_input(filename)
-    ecg_metrics = metrics(ecg_raw_data)
+    ecg_time, ecg_volt = read_input(filename)
+    print(ecg_time)
+    print(ecg_volt)
+    ecg_metrics = metrics(ecg_time, ecg_volt)
     output_file(ecg_metrics)
 
 
