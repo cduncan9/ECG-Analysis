678ea28d346748204c109cd47388fad9145d4a6e
Canyon Duncan <canyon.duncan@duke.edu>
2020-06-08 16:19:17 -0400

    Wrote a test function for the check_data function. This uses parametrization to test multiple data types.

--- test_ecg_analysis.py
+++ test_ecg_analysis.py
@@ -6,3 +6,14 @@ def test_split_data():
     expected = '1', '2'
     answer = split_data('1, 2')
     assert answer == expected
+
+
+@pytest.mark.parametrize("time, volt, expected", [
+                        ('1', '1', True),
+                        ('1', '', False),
+                        ('1', 'Hello', False),
+                        ('0.5', '-1.7', True)])
+def test_check_data(time, volt, expected):
+    from ecg_analysis import check_data
+    answer = check_data(time, volt)
+    assert answer == expected
